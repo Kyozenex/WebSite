80bf5ed1e4f94bca3d969f658f98757d00214990
Klyshee <kirillzujkov131@gmail.com>
2025-06-29 21:07:10 +0300

    коммит

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template
-
+from flask import Flask, request, send_file
+from pytube import YouTube
+import os
 
 app = Flask(__name__)
 
-@app.route("/index")
-@app.route("/")
-def index():
-    return render_template('index.html')
-
-
-
+@app.route('/youtube', methods=['POST'])
+def youtube_audio():
+    url = request.json.get('url')  # Получаем ссылку из запроса
+    yt = YouTube(url)
+    audio = yt.streams.filter(only_audio=True).first()  # Берём аудиопоток
+    audio.download(filename="temp_audio.mp3")  # Скачиваем временный файл
+    return send_file("temp_audio.mp3")  # Отправляем его пользователю
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
